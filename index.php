a7242a63fba56cda506e9f9bebb36aabf57d8818
Zehsuz <102039842+Zehsuz@users.noreply.github.com>
2022-04-11 12:57:47 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Записная книга</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    if(mt_rand(0,1)){
+    ?>
+    <div style="color: blue">хуй</div>
+    <?php
+    } else{
+    ?>
+        <div style="color: red">хуй</div>
+        <?php
+}
+    ?>
+</body>
+</html>
